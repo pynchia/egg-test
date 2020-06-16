b31f5565675917370dedcfbab639d2b5aa9d38fb
mbb <pynchia@gmail.com>
2020-06-16 13:04:49 +0200

    stuck

--- app/static/js/main.js
+++ app/static/js/main.js
@@ -1,12 +1,19 @@
 const usersURI = "http://localhost:8000/users";
 
+function refreshTable() {
+	let rows = getData(usersURI);
+	rows.then(data => {
+		let context = {users: data};
+		const compiledHTML = users_template(context);
+		document.getElementById("usertable").innerHTML = compiledHTML;
+	});
+}
+
 const add_button = document.getElementById("addtodb");
 add_button.onclick = event => {
 	const formEls = document.getElementsByClassName("formfield");
-	// console.log(formToJSON(formEls));
 	postData(usersURI, formToJSON(formEls))
 	.then(data => {
-		console.log(data); // JSON data parsed by `response.json()` call
 		refreshTable()
 	});
 }
@@ -21,19 +28,24 @@ function updateSortByEl(sort) {
 	sortByEl.innerHTML = `Sorting by ${sort.column} ${sort.direction}`;
 }
 
-function refreshTable() {
-	let rows = getData(usersURI);
-	rows.then(data => {
-		console.log(data);
-		let context = {users: data};
-		const compiledHTML = users_template(context);
-		document.getElementById("usertable").innerHTML = compiledHTML;
+function setupSorting() {
+	let sort_cols = document.getElementsByClassName("sortcol");
+	// None of the following works
+	for (var el of sort_cols) {
+		console.log("off");
+	};
+	[].forEach.call(sort_cols, function(el) {
+		console.log("out");
 	});
+	for (var i=0; i<sort_cols.length; i++) {
+		console.log("over");
+	};
 }
 
 function main() {
-	updateSortByEl(current_sort);
 	refreshTable();
+	setupSorting();
+	updateSortByEl(current_sort);
 }
 
 let users_template = Handlebars.compile(document.getElementById("usertemplate").innerHTML);
