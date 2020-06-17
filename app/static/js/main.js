05467d5e954e0cdf089039dcdf5e6760bb8ac5eb
mbb <pynchia@gmail.com>
2020-06-17 11:45:43 +0200

    sorting w/o GET data and generating entries only in table

--- app/static/js/main.js
+++ app/static/js/main.js
@@ -1,22 +1,28 @@
+
 const usersURI = "http://localhost:8000/users";
 
+let data = [
+ {
+ 	name: "Charlie",
+ 	birth: "01-01-1956",
+ 	email: "charlie@angels.com",
+ 	children: 0
+ },
+ {
+ 	name: "Jill",
+ 	birth: "02-02-1962",
+ 	email: "jill@angels.com",
+ 	children: 1
+ }
+];
+
 function refreshTable() {
-	let rows = getData(usersURI);
-	rows.then(data => {
-		let context = {users: data};
-		const compiledHTML = users_template(context);
-		document.getElementById("usertable").innerHTML = compiledHTML;
-	});
+	let context = {users: data};
+	const users_template = Handlebars.compile(document.getElementById("usertemplate").innerHTML);
+	const compiledHTML = users_template(context);
+	document.getElementById("userentries").innerHTML = compiledHTML;
 }
 
-const add_button = document.getElementById("addtodb");
-add_button.onclick = event => {
-	const formEls = document.getElementsByClassName("formfield");
-	postData(usersURI, formToJSON(formEls))
-	.then(data => {
-		refreshTable()
-	});
-}
 
 const current_sort = {
 	column: "Name",
@@ -32,14 +38,26 @@ function setupSorting() {
 	let sort_cols = document.getElementsByClassName("sortcol");
 	// None of the following works
 	for (var el of sort_cols) {
-		console.log("off");
+		el.onclick = event => {
+			console.log();
+			if (current_sort.column === event.target.id) {
+				current_sort.direction = current_sort.direction === "Asc" ? "Desc": "Asc";
+				console.log(current_sort.direction);
+			} else {
+				current_sort.direction = "Asc";
+				current_sort.column = event.target.id;
+				console.log(current_sort.column);
+			}
+			updateSortByEl(current_sort);
+			refreshTable();
+		}
 	};
-	[].forEach.call(sort_cols, function(el) {
-		console.log("out");
-	});
-	for (var i=0; i<sort_cols.length; i++) {
-		console.log("over");
-	};
+	// [].forEach.call(sort_cols, function(el) {
+	// 	console.log("out");
+	// });
+	// for (var i=0; i<sort_cols.length; i++) {
+	// 	console.log("over");
+	// };
 }
 
 function main() {
@@ -47,7 +65,5 @@ function main() {
 	setupSorting();
 	updateSortByEl(current_sort);
 }
-
-let users_template = Handlebars.compile(document.getElementById("usertemplate").innerHTML);
 
 main();
